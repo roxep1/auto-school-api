93353bbfd7f4f76802b078afb427e24bf0658f42
Philipp <isip_f.v.bashkir@mpt.ru>
2021-09-13 22:15:51 +0300

    Initial commit (w/ Heroku support!)

---
+++ auto-school-api/src/main/kotlin/com/bashkir/Application.kt
@@ -0,0 +1,13 @@
+package com.bashkir
+
+import io.ktor.application.*
+import com.bashkir.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
