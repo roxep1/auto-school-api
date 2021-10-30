45c65a1248d2e0a46927a1534bebb744427f7eb9
Philipp <isip_f.v.bashkir@mpt.ru>
2021-10-30 20:35:48 +0300

    authentication

--- auto-school-api/src/main/kotlin/com/bashkir/Application.kt
+++ auto-school-api/src/main/kotlin/com/bashkir/Application.kt
@@ -19,10 +19,10 @@ fun main(args: Array<String>): Unit =
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     connectDatabase()
+    configureKoin()
     configureAuthentication()
     configureRouting()
     configureSerialization()
-    configureKoin()
 
     employeesRouting()
 
